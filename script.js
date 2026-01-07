d8358991725dc3afcd93aa3846d44af33501ca15
Komalsingh003 <153943927+Komalsingh003@users.noreply.github.com>
2026-01-07 15:43:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
     //Write your code here
+const form = document.getElementById("form1");
+
+  const firstName = form.fname.value.trim();
+  const lastName = form.lname.value.trim();
+	
+  const fullName = firstName + " " + lastName;
+	
+  alert(fullName.trim());
+  return false;
+}
 
 }
